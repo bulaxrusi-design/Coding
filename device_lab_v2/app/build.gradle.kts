0fbd3593242034fb48131bcd4fccd78c06d16de8
bulaxrusi-design <bulaxrusi@gmail.com>
2026-07-30 19:19:09 +0400

    Bump Device Lab to v2.3.0

--- device_lab_v2/app/build.gradle.kts
+++ device_lab_v2/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ttclab.chatbridge"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "2.2.0"
+        versionCode = 5
+        versionName = "2.3.0"
     }
 
     buildFeatures {
