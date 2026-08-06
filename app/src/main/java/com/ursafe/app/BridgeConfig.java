dce38a51d6ab6220bac9e4e88b9c4915da372cc2
bulaxrusi-design <bulaxrusi@gmail.com>
2026-08-06 04:58:02 +0400

    feat: add encrypted device bridge config

---
+++ app/src/main/java/com/ursafe/app/BridgeConfig.java
@@ -0,0 +1,13 @@
+package com.ursafe.app;
+
+public final class BridgeConfig {
+    public static final String REPOSITORY = "bulaxrusi-design/Coding";
+    public static final String BRANCH = "ursafe-device-bridge";
+    public static final String RAW_BASE =
+            "https://raw.githubusercontent.com/bulaxrusi-design/Coding/ursafe-device-bridge/";
+    public static final String TERMUX_PACKAGE = "com.termux";
+    public static final String TERMUX_PERMISSION = "com.termux.permission.RUN_COMMAND";
+    public static final String ACTION_LOCAL_RESULT = "com.ursafe.app.TERMUX_RESULT_V05";
+
+    private BridgeConfig() {}
+}
